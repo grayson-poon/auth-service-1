54e23a274150fc70d4482425f63a953ae2a792c8
Grayson Poon <grayson.a.poon@gmail.com>
2023-02-26 23:04:12 -0800

    change to standard method to retrieve env variables

--- firebase.py
+++ firebase.py
@@ -6,10 +6,10 @@ load_dotenv()
 firebase_credentials = json.dumps({
     "type": "service_account",
     "project_id": "user-management-api-1",
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY"),
     "client_email": "firebase-adminsdk-jdm4m@user-management-api-1.iam.gserviceaccount.com",
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
@@ -17,12 +17,12 @@ firebase_credentials = json.dumps({
 })
 
 firebase_config = json.dumps({
-    "apiKey": os.getenv("FIREBASE_API_KEY"),
+    "apiKey": os.environ.get("FIREBASE_API_KEY"),
     "authDomain": "user-management-api-1.firebaseapp.com",
     "projectId": "user-management-api-1",
     "storageBucket": "user-management-api-1.appspot.com",
-    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
-    "appId": os.getenv("FIREBASE_APP_ID"),
-    "measurementId": os.getenv("MEASUREMENT_ID"),
-    "databaseURL": os.getenv("DATABASE_URL")
+    "messagingSenderId": os.environ.get("MESSAGING_SENDER_ID"),
+    "appId": os.environ.get("FIREBASE_APP_ID"),
+    "measurementId": os.environ.get("MEASUREMENT_ID"),
+    "databaseURL": os.environ.get("DATABASE_URL")
 })
