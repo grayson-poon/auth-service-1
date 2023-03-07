877a52614dcbbbfdb2e81d3a47d6e558168f9861
Grayson Poon <grayson.a.poon@gmail.com>
2023-03-07 12:56:19 -0800

    add new verify router to main app

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import login, signup, user
+from routers import login, signup, user, verify
 
 ALLOW_ALL = ["*"]
 app = FastAPI()
 app.include_router(login.router)
 app.include_router(signup.router)
 app.include_router(user.router)
+app.include_router(verify.router)
 app.add_middleware(
    CORSMiddleware,
    allow_origins=ALLOW_ALL,
